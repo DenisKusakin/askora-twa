db59956abe825298b48761fc84be43d793671bc2
Denis_Kusakin <kusakindenis@gmail.com>
2024-10-20 22:41:33 +0200

    ui changes

--- components/question-component.tsx
+++ components/question-component.tsx
@@ -53,7 +53,7 @@ export function QuestionView({question, showButtons = false, showFrom = false, s
         </div>
         {!question.isClosed && showButtons && <div className="card-actions justify-end mb-2 mr-2">
             <button className={"btn btn-secondary btn-sm"} onClick={onRejectClick}>Reject</button>
-            <Link href={`/my-question?id=${question.id}command=reply`} className={"btn btn-primary btn-sm"}>Reply</Link>
+            <Link href={`/my-question?id=${question.id}&command=reply`} className={"btn btn-primary btn-sm"}>Reply</Link>
         </div>}
         {question.replyContent}
     </div>
