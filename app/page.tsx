65879e7417523952bc5d6180589d1705a2da8d6a
Denis_Kusakin <kusakindenis@gmail.com>
2024-11-03 15:06:00 +0100

    test twa start param

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,25 @@
 'use client';
 
 import MyProfile from "@/components/v2/my-profile";
-import Profile from "@/components/v2/profile";
-import {Address} from "@ton/core";
 import {useStoreClientV2} from "@/components/hooks/use-store-client";
 import {$tgStartParam} from "@/stores/tg-store";
+import {useEffect} from "react";
+import {useRouter} from "next/navigation";
 
 export default function Home() {
     const tgStartParam = useStoreClientV2($tgStartParam)
+    const router = useRouter()
+
+    useEffect(() => {
+        if (tgStartParam !== undefined && !tgStartParam.isLoading && tgStartParam.startParam != null) {
+            router.push(`/account?id=${tgStartParam.startParam}`)
+        }
+    }, [router, tgStartParam]);
 
     if (tgStartParam === undefined || tgStartParam.isLoading) {
         return <div className={"w-full mt-[50%] flex justify-center"}>
             <div className={"loading loading-ring w-[125px] h-[125px]"}></div>
         </div>
     }
-    let comp = <MyProfile/>;
-    if (tgStartParam.startParam != null) {
-        comp = <Profile owner={Address.parse(tgStartParam.startParam)}/>
-    }
-    return <div>
-        <p className={"text text-sm"}>Start: {tgStartParam?.startParam}</p>
-        {comp}
-    </div>
+    return <MyProfile/>
 }
