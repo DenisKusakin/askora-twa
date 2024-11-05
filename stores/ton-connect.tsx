da892a2f9dcc921d27075c19ce575b0de9bd8bf5
Denis_Kusakin <kusakindenis@gmail.com>
2024-11-05 18:24:39 +0100

    migrate tonconnect-manifest.json to the root

--- stores/ton-connect.tsx
+++ stores/ton-connect.tsx
@@ -5,9 +5,7 @@ import {TonConnectUI} from "@tonconnect/ui";
 let tonConnectUI2 = null
 if (typeof window != 'undefined') {
     tonConnectUI2 = new TonConnectUI({
-        // manifestUrl: 'https://gist.githubusercontent.com/DenisKusakin/de24b720a1cf30fb37a6e3f081e534c1/raw/5ab9448366149533f586cb77c43aa66768a455cf/gistfile1.txt',
-        // TODO: Make it available under root
-        manifestUrl: 'https://deniskusakin.github.io/askora-twa/manifest.json',
+        manifestUrl: 'https://askora-twa.vercel.app/tonconnect-manifest.json',
     })
 }
 
