a241d41ec1cf8ddb3d2e911678aa087da6d32a67
Denis_Kusakin <kusakindenis@gmail.com>
2024-11-07 18:04:50 +0100

    first attempt for tg connection

--- services/api.ts
+++ services/api.ts
@@ -1,14 +1,21 @@
-//const basePath = "https://sc7b1v7bywdj.share.zrok.io"
-const basePath = "http://localhost:3002"
+const basePath = "https://sc7b1v7bywdj.share.zrok.io"
+// const basePath = "http://localhost:3002"
 
 export async function fetchSubscriptions(tgId: string): Promise<string[]> {
-    const response = await fetch(`${basePath}/subscriptions?tg_id=${tgId}`)
+    const response = await fetch(`${basePath}/subscriptions?tg_id=${tgId}`, {
+        headers: {
+            'skip_zrok_interstitial': 'true'
+        }
+    })
     return response.json().then(x => x.members)
 }
 
 export async function subscribe(tgId: string, walletAddr: string): Promise<void> {
     await fetch(`${basePath}/subscribe`, {
         method: 'POST',
-        body: JSON.stringify({tgId: tgId, walletAddr})
+        body: JSON.stringify({tgId: tgId, walletAddr}),
+        headers: {
+            'skip_zrok_interstitial': 'true'
+        }
     })
 }
