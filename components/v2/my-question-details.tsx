18bede0b8e1e46eb0ce31c3f8b40f8d2d835b457
Denis_Kusakin <kusakindenis@gmail.com>
2024-11-09 15:38:42 +0100

    small fix

--- components/v2/my-question-details.tsx
+++ components/v2/my-question-details.tsx
@@ -5,7 +5,9 @@ import {$myAssignedQuestions} from "@/stores/questions-store";
 export default function MyQuestionDetails({id}: { id: number }) {
     const data = useStoreClient($myAssignedQuestions)
     if (data?.isLoading) {
-        return <div className={"pt-10 loading loading-dots loading-lg"}></div>
+        return <div className={"w-full mt-[50%] flex justify-center"}>
+            <div className={"loading loading-ring w-[125px] h-[125px]"}></div>
+        </div>
     }
     const qData = data?.data.find(x => x.id === id)
     if (qData == null) {
