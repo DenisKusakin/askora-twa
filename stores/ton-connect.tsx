4352b8e13ced4b6efd2bc0634ce7a70cc1462d4d
Denis_Kusakin <kusakindenis@gmail.com>
2024-11-11 16:15:34 +0100

    tonconnect change

--- stores/ton-connect.tsx
+++ stores/ton-connect.tsx
@@ -6,6 +6,9 @@ let tonConnectUI2 = null
 if (typeof window != 'undefined') {
     tonConnectUI2 = new TonConnectUI({
         manifestUrl: 'https://askora-twa.vercel.app/tonconnect-manifest.json',
+        actionsConfiguration: {
+            twaReturnUrl: 'https://t.me/AskoraBot/app'
+        }
     })
 }
 
