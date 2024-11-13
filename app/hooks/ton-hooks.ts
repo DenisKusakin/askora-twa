c321aa31bf892587cbb2e55276766c45dda07549
Denis_Kusakin <kusakindenis@gmail.com>
2024-11-13 12:28:52 +0100

    fix blinking wallet connection status

--- app/hooks/ton-hooks.ts
+++ app/hooks/ton-hooks.ts
@@ -1,16 +1,20 @@
-import {useTonAddress} from "@tonconnect/ui-react";
+import {useTonAddress, useTonConnectUI} from "@tonconnect/ui-react";
 import {Address} from "@ton/core";
 import {useEffect, useState} from "react";
 
-export function useMyConnectedWallet(): Address | null {
-    const [res, setRes] = useState<null | Address>(null)
+export function useMyConnectedWallet(): Address | null | undefined {
+    const [res, setRes] = useState<undefined | null | Address>(undefined)
     const myConnectedWallet = useTonAddress()
+    const [tonConnectUI] = useTonConnectUI()
+
     useEffect(() => {
-        if (myConnectedWallet === '') {
-            setRes(null)
-        } else {
-            setRes(Address.parse(myConnectedWallet))
-        }
-    }, [myConnectedWallet]);
+        tonConnectUI.connectionRestored.then(restored => {
+            if (!restored || myConnectedWallet === '') {
+                setRes(null)
+            } else {
+                setRes(Address.parse(myConnectedWallet))
+            }
+        })
+    }, [myConnectedWallet, tonConnectUI]);
     return res
 }
