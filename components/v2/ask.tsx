d957233b347ebe356440ef8bb518acd6f861da16
Denis_Kusakin <kusakindenis@gmail.com>
2024-11-13 11:04:37 +0100

    one more attempt

--- components/v2/ask.tsx
+++ components/v2/ask.tsx
@@ -9,12 +9,13 @@ import {useTonConnectUI} from "@tonconnect/ui-react";
 import {createQuestionTransaction} from "@/components/utils/transaction-utils";
 import {useMyConnectedWallet} from "@/app/hooks/ton-hooks";
 import {TgMainButtonContext, TgMainButtonProps} from "@/app/context/tg-main-button-context";
+import {MyTgContext} from "@/app/context/tg-context";
 
 export default function Ask({addr}: { addr: Address }) {
     const [text, setText] = useState("")
     const myConnectedWallet = useMyConnectedWallet()
-    console.log("My Connected wallet", myConnectedWallet?.toString())
     const [accountInfo, setAccountInfo] = useState<{ isLoading: boolean, data?: AccountInfo }>({isLoading: true})
+    const tgInitData = useContext(MyTgContext).info?.tgInitData
     const [tonConnectUI] = useTonConnectUI();
     const tgMainButton = useContext(TgMainButtonContext)
     const transactionFee = toNano(0.1)
@@ -76,6 +77,7 @@ export default function Ask({addr}: { addr: Address }) {
     const onConnectClick = () => {
         tonConnectUI.openModal()
     }
+    const isInTelegram = !(tgInitData == null || tgInitData === '')
 
     return <>
         {isSuccessDialogVisible && <TransactionSucceedDialog content={transactionSuccessLinks}/>}
@@ -91,7 +93,7 @@ export default function Ask({addr}: { addr: Address }) {
             </div>
             <textarea className={"textarea textarea-bordered textarea-lg mt-4 w-full h-[200px]"} value={text}
                       onChange={e => setText(e.target.value)}/>
-            {myConnectedWallet != null && <button disabled={isDisabled} onClick={onSubmit}
+            {myConnectedWallet != null && !isInTelegram && <button disabled={isDisabled} onClick={onSubmit}
                                                                    className={"btn btn-primary btn-block btn-lg mt-4"}>Submit
             </button>}
             {myConnectedWallet === null &&
