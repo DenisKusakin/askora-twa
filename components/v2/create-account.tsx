ddd0b299756b772d41c1d0d601c5722dc0fed89d
Denis_Kusakin <kusakindenis@gmail.com>
2024-11-15 20:30:36 +0100

    test fix

--- components/v2/create-account.tsx
+++ components/v2/create-account.tsx
@@ -58,7 +58,7 @@ export default function CreateAccount() {
                         defaultValue={isNaN(price) ? '' : price}
                         type={"number"}
                         step={"0.01"}
-                        lang={"en"}
+                        lang={"en-150"}
                         inputMode="decimal"
                         min={"0"}
                         className={`input text-5xl font-bold w-full text-center`}
