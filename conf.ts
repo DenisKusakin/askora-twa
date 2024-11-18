2c8450dd5fd692a9fab3ad5ebc000f9a20b27c42
Denis_Kusakin <kusakindenis@gmail.com>
2024-11-18 10:27:53 +0100

    rollout to prod!

--- conf.ts
+++ conf.ts
@@ -4,6 +4,6 @@ export const API_BASE_PATH = "https://askora-backend-bffb2.ondigitalocean.app"
 // export const API_BASE_PATH = "http://localhost:3002"
 export const APP_CONTRACT_ADDR = Address.parse("EQCJgNytND4K9ydIx3_kn0kBCi3BWWCyMZoIBsYvT8c5JngA")
 
-export const TON_API_ENDPOINT = 'https://testnet.toncenter.com/api/v2/jsonRPC'
-export const TONVIEWER_BASE_PATH = "https://testnet.tonviewer.com"
-export const TON_API_TOKEN = "2de325fb6696fff5c78ea8da9bc0abc800f1133a6723c7d0fd953e729f9c9ace"
+export const TON_API_ENDPOINT = 'https://toncenter.com/api/v2/jsonRPC'
+export const TONVIEWER_BASE_PATH = "https://tonviewer.com"
+export const TON_API_TOKEN = "2c523789987b1d920ac28b117abe1db16db5f813e8cea92d1b22e9ba5d21f7ff"
